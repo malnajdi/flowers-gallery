1537bbc34202723d0bb5f81a4d6b8c8f0134b66b
Mohammed Y. Alnajdi <mohdnagfy@gmail.com>
2020-07-26 08:51:47 +0300

    lesson 4: create apps

--- flowersgallery/flowersgallery/urls.py
+++ flowersgallery/flowersgallery/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view())
+    path('', include('gallery.urls'))
 ]
