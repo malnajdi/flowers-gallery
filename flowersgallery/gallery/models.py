888d0b074a60ef7b2596d29a1cba7ccc94f86aa4
Mohammed Y. Alnajdi <mohdnagfy@gmail.com>
2020-07-27 09:20:19 +0300

    lesson 5: our first model flower

--- flowersgallery/gallery/models.py
+++ flowersgallery/gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Flower(models.Model):
+    '''
+    title
+    image
+    description
+    '''
+    title = models.CharField(max_length=100)
+    image = models.ImageField()
+    description = models.TextField()
+
