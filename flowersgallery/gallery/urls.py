81452a1daa20b5c3739fc8130a600d91b15d3529
Mohammed Y. Alnajdi <mohdnagfy@gmail.com>
2022-05-09 19:35:32 +0300

    upgrade requirements and create base template

--- flowersgallery/gallery/urls.py
+++ flowersgallery/gallery/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'gallery'
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name="flower-home"),
     path('flower/<int:pk>/', views.FlowerDetailView.as_view(), name='flower-detail'),
     path('flower/create/', views.FlowerCreateView.as_view(), name='flower-create'),
     path('flower/<int:pk>/update/', views.FlowerUpdateView.as_view(), name='flower-update'),
